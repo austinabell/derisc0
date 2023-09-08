60d190be371a2ee6dd0a26f020c1c59708ee87de
Austin Abell <austinabell8@gmail.com>
2023-09-07 21:07:44 -0400

    fix example

--- examples/basic/methods/guest/src/main.rs
+++ examples/basic/methods/guest/src/main.rs
@@ -6,6 +6,6 @@ use derisc0::Binary;
 
 derisc0::entry!(some_method);
 
-fn some_method(a: u32, b: u32) -> Result<Binary<u32>, &'static str> {
-    a.checked_mul(b).map(Binary).ok_or("integer overflow")
+fn some_method(a: u32) -> Result<Binary<u32>, &'static str> {
+    a.checked_mul(a).map(Binary).ok_or("integer overflow")
 }
