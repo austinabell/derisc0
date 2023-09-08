7883350ea4c0e579f7ab9ff924686fe9455fdff7
Austin Abell <austinabell8@gmail.com>
2023-09-07 21:48:37 -0400

    update example for new version

--- examples/basic/host/src/main.rs
+++ examples/basic/host/src/main.rs
@@ -3,21 +3,20 @@
 // `METHOD_NAME_ID` with `MULTIPLY_ID`
 use methods::{METHOD_NAME_ELF, METHOD_NAME_ID};
 use risc0_zkvm::{
-    default_executor_from_elf,
     serde::{from_slice, to_vec},
-    ExecutorEnv,
+    Executor, ExecutorEnv,
 };
 
 fn main() {
     let env = ExecutorEnv::builder()
-    // Send a & b to the guest
-    .add_input(&to_vec(&3).unwrap())
-    .add_input(&to_vec(&4).unwrap())
-    .build()
-    .unwrap();
+        // Send a & b to the guest
+        .add_input(&to_vec(&3).unwrap())
+        .add_input(&to_vec(&4).unwrap())
+        .build()
+        .unwrap();
 
     // Next, we make an executor, loading the (renamed) ELF binary.
-    let mut exec = default_executor_from_elf(env, METHOD_NAME_ELF).unwrap();
+    let mut exec = Executor::from_elf(env, METHOD_NAME_ELF).unwrap();
 
     // Run the executor to produce a session.
     let session = exec.run().unwrap();
